ad9f4d82055dd187b52e39577478f5408ed1b2bb
Bernardo Farah <ber.farah@gmail.com>
2015-03-24 20:25:13 -0700

    Initial commit

---
+++ software.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+brew tap caskroom/homebrew-cask
+
+# essentials
+echo "Installing essentials"
+brew cask install \
+cloud \
+bettertouchtool \
+seil \
+bartender \
+dashlane \
+vlc \
+sublercli \
+subler \
+spotify \
+skype \
+music-manager \
+google-drive \
+flux \
+typed \
+tunnelblick
+
+# gaming
+brew cask install \
+steam \
+teamspeak-client \
+dolphin \
+desmume
+
+# browsers
+brew cask install \
+firefox \
+google-chrome \
+google-chrome-canary \
+opera
+
+# development
+echo "Installing development tools"
+brew cask install \
+sublime-text-3 \
+iterm2 \
+virtualbox \
+transmit
+
+brew install \
+boot2docker \
+docker
+
+echo "Installing libraries"
+brew install \
+wget \
+imagemagick \
+jpeg \
+libpng \
+libtiff \
+webp
+
+echo "Installing build tools"
+brew install \
+pcre \
+autoconf \
+apple-gcc42 \
+ruby-build \
+makedepend
+
+echo "Installing languages & databases"
+brew install \
+go \
+rbenv \
+watchman \
+node \
+python3 \
+postgresql \
+sqlite
