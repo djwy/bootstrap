7c3715ea9ceea9a0a5bd936aaefd59798eb442e3
Bernardo Farah <ber.farah@gmail.com>
2015-03-24 22:20:09 -0700

    Fixing small typo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Oh-My-Zsh
-$ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
